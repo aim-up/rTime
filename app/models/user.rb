3eded4c9b71ed167241876636970ccbca7ef9b92
Daniel Asberry <Endial1988@gmail.com>
2011-11-11 00:43:23 -0500

    restricted email to repro domains

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   has_secure_password
   attr_accessor :comment
+  
+  email_regex = /\A[\w+\-.]+@reprographix\.com+\z/i
+  
   attr_accessible :email, :password, :password_confirmation,
                   :name,  :status
   validates :password,    :presence     => { :on => :create },
                           :confirmation => { :on => :create }
+  validates :email,       :format       => { :with => email_regex }
   has_many  :punches,     :dependent => :destroy
-  STATUSES = ['In', 'Out', 'Break 1', 'Break 2', 'Lunch']
+  STATUSES = ['In', 'Out', 'Break 1', 'Break 2', 'Lunch', 'Holiday']
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,32 @@
 require 'spec_helper'
 
 describe User do
-
+  
+  before(:each) do
+    @attr = { :name                  => "Joseph Khamel",
+              :email                 => "joe.khamel@reprographix.com",
+              :password              => 'pa55w0rD',
+              :password_confirmation => 'pa55w0rD' }
+  end
+  
   describe "password encryption" do
 
     it "should authenticate with matching username and password" do
-      user = Factory(:user, :email => 'frank@gmail.com', :password => 'secret', :password_confirmation => 'secret')
-      User.find_by_email('frank@gmail.com').try(:authenticate, 'secret').should == user
+      user = User.create! @attr
+      User.find_by_email('joe.khamel@reprographix.com').try(:authenticate, 'pa55w0rD').should == user
+    end
+  end
+  
+  describe "Validation" do
+    
+    it "should reject all email domains but @reprographix.com" do
+      outside_user = User.new(@attr.merge(:email => 'joe.khamel@notrepro.com'))
+      outside_user.should_not be_valid
+    end
+    
+    it "should allow emails from @reprographix.com" do
+      inside_user  = User.new @attr
+      inside_user.should be_valid
     end
   end
 end
